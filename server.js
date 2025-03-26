19da4a799901fbdd8baec2cca9762b68791ef44f
Yazhinids28 <155945727+Yazhinids28@users.noreply.github.com>
2025-03-26 12:02:35 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+// Smooth scrolling effect
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      window.scrollTo({
+        top: targetElement.offsetTop - 50,
+        behavior: 'smooth'
+      });
+    });
+  });
+  
